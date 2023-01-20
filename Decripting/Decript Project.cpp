352a1cdfb8ccfc9926b1e1016f4b0b17bf24385b
Mihail Jordanov <103178509+BojeMoii@users.noreply.github.com>
2023-01-20 23:21:30 +0200

    Added README

--- Decripting/Decript Project.cpp
+++ Decripting/Decript Project.cpp
@@ -1,3 +1,16 @@
+/**
+*
+* Solution to course project # 2
+* Introduction to programming course
+* Faculty of Mathematics and Informatics of Sofia University
+* Winter semester 2022/2023
+*
+* @author Mihail Yordanov
+* @idnumber 7MI0600198 @compiler VC
+*
+* Decripting file
+*
+*/
 #include <iostream>
 #include <cstring>
 #include <fstream>
